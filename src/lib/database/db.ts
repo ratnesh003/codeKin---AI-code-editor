45dbd39154fafc317394c1a859df4f76cc75afba
Ratnesh Pasi <ratneshpasi03@gmail.com>
2025-03-02 00:08:12 +0530

    update database name and enhance middleware configuration for deployment solution

--- src/lib/database/db.ts
+++ src/lib/database/db.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
     cached.promise = 
         cached.promise || 
         mongoose.connect(MONGODB_URL, {
-            dbName: 'pixify-database', bufferCommands: false
+            dbName: 'code-kin-database', bufferCommands: false
         })
 
     cached.conn = await cached.promise;
--- src/middleware.ts
+++ src/middleware.ts
@@ -21,5 +21,12 @@ export const config = {
     '/settings/:path*',
     '/profile/:path*',
     '/files/:path*'
-  ]
+  ],
+  runtime: "nodejs",
+  unstable_allowDynamic: [
+      // allows a single file
+      "/src/db/lib/db.ts",
+      // use a glob to allow anything in the function-bind 3rd party module
+      "/node_modules/mongoose/dist/**",
+  ],
 };
